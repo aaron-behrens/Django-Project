03cd79cb560b220324a878f781df9265052861bf
aaron-behrens <ab2180@icloud.com>
2024-06-14 13:38:52 +0000

    Delete dbmovies directory

--- dbmovies/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import MovieList, MovieDetail
-
-urlpatterns = [
-    path('movie/', MovieList.as_view()),
-    path('movie/<int:pk>/', MovieDetail.as_view()) 
-]
-
